49c3539f11c2b1831536c0662538aedd2742dc4d
Esteban Volentini <evolentini@equiser.com.ar>
2021-04-10 12:40:11 -0300

    Se agregan los datos del alumno PERMINGEAT, Alejandro

--- src/alumnos.c
+++ src/alumnos.c
@@ -46,4 +46,13 @@ bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno) {
     return (resultado >= 0);
 }
 
+bool AlejandroPermingeat(char * cadena, size_t espacio) {
+    const struct alumno_s alumno = {
+        .apellidos = "PERMINGEAT",
+        .nombres = "Alejandro",
+        .documento = "00.000.000",
+    };
+
+    return SerializarAlumno(cadena, espacio, &alumno);
+}
 /*=====[Implementations of private functions]================================*/
