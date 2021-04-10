f538cfdbc023b7775d36cbaf5e23cb99fc0ed445
Alejandro <alejandrorougier@gmail.com>
2021-04-10 15:50:13 -0300

    "Se agregan los datos del alumno ROUGIER,  Alejandro"

--- src/alumnos.c
+++ src/alumnos.c
@@ -46,4 +46,14 @@ bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno) {
     return (resultado >= 0);
 }
 
+ bool AlejandroRougier(char * cadena, size_t espacio) {
+        const struct alumno_s alumno = {
+            .apellidos = "ROUGIER",
+            .nombres = "Omar Alejandro",
+            .documento = "22.661.059",
+        };
+
+        return SerializarAlumno(cadena, espacio, &alumno);
+    }
+
 /*=====[Implementations of private functions]================================*/
