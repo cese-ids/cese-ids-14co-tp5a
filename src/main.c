1824465eb4c12cc46ac57affb5172bf737913ffb
josueelias9 <30317390+josueelias9@users.noreply.github.com>
2021-04-24 05:00:26 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -132,6 +132,12 @@ int main(int argc, char *argv[]) {
     } else {
         printf("No se pudo serializar el alumno\r\n");
     }
+    
+    if (JosueHuaman(cadena, sizeof(cadena))) {
+    printf("Alumno: %s\r\n", cadena);
+    } else {
+    printf("No se pudo serializar el alumno\r\n");
+    }
 
 
     return 0;
